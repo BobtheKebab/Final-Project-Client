930f9aaa31aaf9ac4d3e0301621fa290abbd5c90
Ahbab Ashraf <ahbab.ashraf@gmail.com>
2022-07-11 16:37:24 -0400

    Can add campus

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ import {
   NewStudentContainer
 } from './components/containers';
 
+// it won't import if i don't do this
+import NewCampusContainer from './components/containers/NewCampusContainer';
+
 // if you create separate components for adding/editing 
 // a student or campus, make sure you add routes to those
 // components here
@@ -22,6 +25,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={HomePageContainer} />
         <Route exact path="/campuses" component={AllCampusesContainer} />
+        <Route exact path="/newcampus" component={NewCampusContainer} />
         <Route exact path="/campus/:id" component={CampusContainer} />
         <Route exact path="/students" component={AllStudentsContainer} />
         <Route exact path="/newstudent" component={NewStudentContainer} />
